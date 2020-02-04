bb32c3caab88bdd973b331ad9719192e9478f2c3
AChaffangeon <chaffangeon.adrien@gmail.com>
2020-02-04 16:55:15 +0100

    Display the timer during the break

--- src/ts/views/PauseView.ts
+++ src/ts/views/PauseView.ts
@@ -5,21 +5,35 @@ import { PauseTimeoutEvent } from "../events/PauseTimeoutEvent";
 interface ViewParameter { };
 
 export class PauseView extends View<ViewParameter> {
-    private static readonly PAUSE_DURATION: number = 5000;
+    private static readonly PAUSE_DURATION: number = 5;
     private timer: number;
 
     beforeRender(): void {
         this.node = $("<div>").attr("id", "pause-view");
 
         $("<p>").text("Break!").appendTo(this.node);
+
+        $("<p>").attr("id", "timer-display").appendTo(this.node);
     }
 
     render(): void {
         $("body").append(this.node);
-        this.timer = window.setTimeout(() => {
-            EventManager.emit(new PauseTimeoutEvent());
-            console.log("Pause end");
-        }, PauseView.PAUSE_DURATION);
+
+        let second = PauseView.PAUSE_DURATION;
+        let timerDisplay = $("#timer-display");
+
+        let setTimer = () => {
+            if (second > 0) {
+                timerDisplay.text(second);
+                this.timer = window.setTimeout(setTimer, 1000);
+                console.log(second);
+            } else {
+                EventManager.emit(new PauseTimeoutEvent());
+                console.log("Pause end");
+            }
+            second -= 1;
+        };
+        setTimer();
     }
 
     destroy(): void {
