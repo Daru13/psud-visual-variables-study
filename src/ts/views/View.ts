0e4b156f30564ef00aae87e37e7850a3052cb4bc
AChaffangeon <chaffangeon.adrien@gmail.com>
2020-02-04 11:25:45 +0100

    Create abstract class View

--- src/ts/views/View.ts
+++ src/ts/views/View.ts
@@ -1,3 +1,8 @@
-export abstract class View {
+import "jquery";
 
+export abstract class View<P extends object> {
+    node: JQuery;
+    abstract beforeRender(parameters: P): void
+    abstract render(): void
+    abstract destroy(): void
 }
