cb017c64fe57521383af322c03189d0bb6b727fb
AChaffangeon <chaffangeon.adrien@gmail.com>
2020-02-04 15:21:22 +0100

    Add a final view with a download csv button

---
+++ src/ts/views/FinalView.ts
@@ -0,0 +1,36 @@
+import "jquery";
+import * as FileSaver from "file-saver";
+import { View } from "./View";
+
+interface ViewParameter { fileName: string, csv: string };
+
+export class FinalView extends View<ViewParameter> {
+    beforeRender(parameter: ViewParameter): void {
+        this.node = $("<div>").attr("id", "final-view");
+        
+        $("<p>").text("The end!").appendTo(this.node);
+
+        $("<button>")
+            .attr("id", "download-csv-button")
+            .text("Download csv")
+            .on("click", () => {
+                this.downloadCSVFile(parameter);
+            })
+            .appendTo(this.node);
+    }
+
+    render(): void {
+        $("body").append(this.node);
+    }
+
+    destroy(): void {
+        this.node.remove();
+    }
+
+    downloadCSVFile(parameter: ViewParameter) {
+        let filename = parameter.fileName
+        let csvFileBlob = new Blob([parameter.csv], { type: "text/csv;charset=utf-8" });
+
+        FileSaver.saveAs(csvFileBlob, filename);
+    }
+}
